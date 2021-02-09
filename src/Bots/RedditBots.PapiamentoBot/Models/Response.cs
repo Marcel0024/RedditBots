a488be3c7c4efbe3eb69d0406e1806957f948c34
Marcel <14852157+Marcel0024@users.noreply.github.com>
2021-02-09 19:40:01 +0100

    Added Papiamento Discord Bot

---
+++ src/Bots/RedditBots.PapiamentoBot/Models/Response.cs
@@ -0,0 +1,18 @@
+﻿using RedditBots.PapiamentoBot.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditBots.PapiamentoBot.Models
+{
+    public class Response
+    {
+        public static Response Empty() => new Response();
+
+        public bool MistakeFound { get; set; }
+
+        public Word Mistake { get; set; }
+    }
+}
---
+++ src/Bots/RedditBots.PapiamentoBot/PapiamentoDiscordBot.cs
@@ -0,0 +1,62 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RedditBots.Framework;
+using RedditBots.PapiamentoBot.Settings;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedditBots.PapiamentoBot
+{
+    public class PapiamentoDiscordBot : AbstractPapiamentoBot
+    {
+        private static DiscordSocketClient _discordClient;
+        private readonly BotSetting _botSetting;
+
+        public PapiamentoDiscordBot(
+                ILogger<PapiamentoDiscordBot> logger,
+                IHostEnvironment env,
+                IOptions<MonitorSettings> monitorSettings,
+                IOptions<PapiamentoBotSettings> papiamentoBotSettings)
+        {
+            Logger = logger;
+            Env = env;
+            _botSetting = monitorSettings.Value.Settings.Find(ms => ms.BotName == nameof(PapiamentoDiscordBot)) ?? throw new ArgumentNullException("No bot settings found");
+            PapiamentoBotSettings = papiamentoBotSettings.Value;
+
+            _discordClient = new DiscordSocketClient();
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await _discordClient.LoginAsync(TokenType.Bot, _botSetting.AppSecret);
+            await _discordClient.StartAsync();
+
+            _discordClient.MessageReceived += C_NewCommentsUpdated;
+        }
+
+        private async Task C_NewCommentsUpdated(SocketMessage message)
+        {
+            if (message.Author.IsBot)
+            {
+                return;
+            }
+
+            var response = CheckCommentGrammar(new Models.Request
+            {
+                Content = message.Content,
+                From = $"discord channel {message.Channel.Name}"
+            });
+
+            if (response.MistakeFound)
+            {
+                var replyText = string.Format(_botSetting.DefaultReplyMessage, message.Author.Mention, response.Mistake.Wrong, response.Mistake.Right);
+
+                await message.Channel.SendMessageAsync(replyText);
+            }
+        }
+    }
+}
