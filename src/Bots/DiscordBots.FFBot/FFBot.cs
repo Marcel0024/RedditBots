e9e667ba3869eee34e5fd6fc245b9305befbcd2b
Marcel Croes <marcel.croes@dutchworkz.nl>
2021-02-10 11:18:04 +0100

    Up the odds

--- src/Bots/DiscordBots.FFBot/FFBot.cs
+++ src/Bots/DiscordBots.FFBot/FFBot.cs
@@ -62,7 +62,7 @@ namespace DiscordBots.FFBot
                 await message.Channel.SendMessageAsync("ook Thijs", messageReference: new MessageReference(message.Id));
             }
 
-            else if (_random.Next(0, 100) < 2)
+            else if (_random.Next(0, 1000) < 7)
             {
                 var random = _random.Next(0, 4);
 
