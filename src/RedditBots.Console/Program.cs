346a1e0b7226ffcdbf9946a54c7be001dc9cbe63
Marcel Croes <14852157+Marcel0024@users.noreply.github.com>
2021-06-04 23:33:00 +0200

    Live preparations

--- src/RedditBots.Console/Program.cs
+++ src/RedditBots.Console/Program.cs
@@ -33,10 +33,10 @@ namespace RedditBots
             {
                 services.Configure<MonitorSettings>(hostContext.Configuration.GetSection(nameof(MonitorSettings)));
 
-                //services.AddHanzeMemesBot();
-                //services.AddPeriodicallyBot(hostContext);
+                services.AddHanzeMemesBot();
+                services.AddPeriodicallyBot(hostContext);
                 services.AddPapiamentoBot(hostContext);
-                //services.AddCheerfulBot();
+                services.AddCheerfulBot();
             });
     }
 }
